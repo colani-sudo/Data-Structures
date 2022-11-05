bb6bb17a13d8657fb14151d252d60b802b01cddd
colani-sudo <matsenjwacolani@gmail.com>
2022-11-06 02:32:21 +0800

    linkedlist using templates final solution

--- DSW2Q1LinkedList/DSW2Q1LinkedList/main.cpp
+++ DSW2Q1LinkedList/DSW2Q1LinkedList/main.cpp
@@ -1,49 +1,45 @@
-//
-//  main.cpp
 //  DSW2Q1LinkedList
-//
 //  Created by Colani Matsenjwa on 2022/10/10.
-//
 
-/**
- * A Node class of Doublely Linked List class
- */
 #include <iostream>
 
 using namespace std;
 
 template<class T>
 class Node {
+
+    
   public:
 
   /**
    * Construct a new Node object with no parameter
    */
-  Node() {
-      Node(0, NULL, NULL);
-  }
+    Node() : Node(-1, NULL, NULL){}
 
   /**
    * Construct a new Node object with parameter
-   * @param data: data to put in
+   * @param d data to put in
    */
-  Node(T data){
-      Node(data, NULL, NULL);
-  }
+Node(T d): Node(d, NULL, NULL){}
 
   /**
    * Construct a new Node object with parameters
-   * @param data: data to put in
-   * @param prev: pointer to the previous node
-   * @param next: pointer to the next node
+   * @param d : data to put in
+   * @param p : pointer to the previous node
+   * @param n : pointer to the next node
    */
-  Node(T data, Node<T> *prev, Node<T> *next) {
-      this->data = data;
-      this->next = next;
-      this->prev = prev;
-  }
+Node(T d, Node<T> *p, Node<T> *n) {
+    data = d;
+    next = n;
+    prev = p;
+}
 
-  private:
+Node<T> *getNext(){ return next;}
+Node<T> *getPrev(){ return prev;}
+void setNext(Node<T> *next) { this->next = next;}
+void setPrev(Node<T> *prev) { this->prev = prev;}
+T getData(){ return data;}
+private:
     Node<T> *prev;
     Node<T> *next;
     T data;
@@ -60,61 +56,146 @@ class LinkedList {
    * Construct a new LinkedList object with no parameter
    */
   LinkedList() {
-      head = new Node<T>();
+      head = NULL;
       tail = head;              //just pass the reference of tail
   }
-
+    Node<T> *getHead() { return head;}
+    Node<T> *getTail() { return tail;}
   /**
    * Add a node to the tail of the linked list
-   * @param data: data to put in
+   * @param data data to put in
    */
+  
   void addToTail(T data) {
-      Node newNode = new Node<T>(data);
+      Node<T> *newNode = new Node<T>(data);
       if(head == NULL){
           head = newNode;
           tail = head;
       }
-      Node currNode = head;
+      Node<T> *currNode = head;
       while(currNode != tail){
-          currNode = currNode->next;
+          currNode = currNode->getNext();
       }
-      currNode->next = newNode;
-      newNode->prev = currNode;
+      currNode->setNext(newNode);
+      newNode->setPrev(currNode);
       tail = newNode;
   }
 
   /**
    * Add a node to the head of the linked list
-   * @param data: data to put in
+   * @param data data to put in
    */
   void addToHead(T data) {
-      Node newNode = new Node<T>(data);
-      if(head == NULL){
-          head = newNode;
-          tail = head;
-      }
-      newNode->next = head;
-      head->prev = newNode;
-      head = newNode;
-  }
+    Node<T> *newNode = new Node<T>(data);
+    if(!head)
+    {
+        head = newNode;
+        tail = head;
+    }
+    else
+    {
+        newNode->setNext(head);
+        head->setPrev(newNode);
+        head = newNode;
+    }
+}
 
   /**
    * Delete a node from the linked list with the given data from the head
    * If there are no data to be deleted, then do nothing
-   * @param data: data to delete
+   * @param data data to delete
    */
-  void deleteData(T data) {
-      if(head == NULL) return;
-      
+void deleteData(T data) {
+    Node<T> *tempNode, *tempNode2;
+    if(head == nullptr)
+    {
+        cout << "List is empty.";
+        return;
+    }
+    else if(head->getData() == data)
+    {
+        tempNode = head;
+        if(head->getNext() != nullptr)
+        {
+            head = head->getNext();
+            head->setPrev(nullptr);
+            delete tempNode;
+            tempNode = nullptr;
+        }
+        else{
+            delete head;
+            head = nullptr;
+            delete tempNode;
+            tempNode = nullptr;
+            return;
+        }
+    }
+    tempNode2 = head;
+    while(tempNode->getNext()->getNext() != nullptr)
+    {
+        if(tempNode2->getNext()->getData() == data)
+        {
+            tempNode = tempNode2->getNext();
+            tempNode2->setNext(tempNode->getNext());
+            tempNode->getNext()->setPrev(tempNode2);
+            delete tempNode;
+            tempNode = nullptr;
+            return;
+        }
+        tempNode2 = tempNode2->getNext();
+    }
+
+    if(tempNode2->getNext()->getData() == data)
+    {
+        tempNode = tempNode2->getNext();
+        delete tempNode;
+        tempNode = nullptr;
+        tempNode2->setNext(nullptr);
+        return;
+    }
+    cout << "Element not found" << endl;
   }
 
   /**
    * Delete valid n nodes from the linked list with the given data from the head
    * If there are no more data to be deleted, then just skip
-   * @param data: data to delete
-   * @param n: max number of nodes to delete
+   * @param data data to delete
+   * @param n max number of nodes to delete
    */
-  void deleteData(T data, int n) {}
+  void deleteData(T data, int n) {
+      if(head == NULL){
+          return;
+      }
+      Node<T> *prevNode = NULL;
+      while(n > 0){
+          if(head==tail && head->getData() == data){
+              free(head);
+              tail = head = NULL;
+              n--;
+          }else{
+              Node<T> *currNode = head;
+              
+              Node<T> *tempNode = currNode;
+              if(currNode->getData() == data){
+                  if(currNode == tail){
+                      tail->setPrev(prevNode);
+                      tail = prevNode;
+                      delete tempNode;
+                      n--;
+                  }else{
+                      head = currNode->getPrev();
+                      head->setPrev(currNode->getNext()->getNext());
+                      delete tempNode;
+                      n--;
+                  }
+              }
+              prevNode = currNode;
+              currNode = currNode->getNext();
+          }
+      }
+      delete prevNode;
+      return;
+  }
 
   /**
    * Overload the operator << to print out all the data in the linked list from the head
@@ -124,7 +205,20 @@ class LinkedList {
    * If your linked list data is 5, 4, 3, 2, 1
    * then you should print out "(5, 4, 3, 2, 1)" with the content in quotes but without the quotes
    */
-  friend std::ostream &operator<<(std::ostream &out, LinkedList * n) {}
+  friend std::ostream &operator<<(std::ostream &out, LinkedList<T> *n) {
+      if(n != NULL){
+        
+        Node<T> *currNode = n->getHead();
+          out << "(";
+        while(currNode->getNext() != NULL){
+            out << currNode->getData() << ", ";
+            currNode = currNode->getNext();
+        }
+          
+        out << currNode->getData()<< ")\n";
+      }
+      return out;
+  }
 
   private:
     Node<T> *head = NULL;
@@ -132,6 +226,14 @@ class LinkedList {
 };
 
 int main(){
+    
+    LinkedList<int> *n = new LinkedList<int>();
+    n->addToHead(1);
+    n->addToHead(2);
+    n->addToHead(3);
+    n->addToHead(4);
+    n->addToHead(5);
+    cout << n;
     
     return 0;
 }
